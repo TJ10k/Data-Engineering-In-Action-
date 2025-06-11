96883f21df21ef4689df98442c9e4b27785a662f
TJ10k <58928343+TJ10k@users.noreply.github.com>
2025-06-11 09:44:31 -0400

    Make project a package and clean imports

--- web/app.py
+++ web/app.py
@@ -1,8 +1,4 @@
 from flask import Flask, render_template, request
-from pathlib import Path
-import sys
-
-sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from db.utils import connect_to_db
 import pandas as pd
